4b41d51463ac1d1c6162eddd46f327398bd5619b
mixalych06 <mixalych06@mail.ru>
2023-03-05 23:07:11 +0900

    Token

--- create_bot.py
+++ create_bot.py
@@ -7,7 +7,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv(filename='.env'), encoding="utf-8", override=True)
-TOKEN = os.environ.get('TOKENTEST')
+TOKEN = os.environ.get('TOKEN')
 CHAT_ID = os.environ.get('CHAT_ID')
 ADMIN_ID = os.environ.get('ADMIN_ID')
 
